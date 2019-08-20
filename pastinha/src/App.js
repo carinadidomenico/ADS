4b99164ccc35fe73e09c4b6ec939c3aefdcec489
carinadidomenico <carinadidomenico@gmail.com>
2019-08-20 17:08:55 -0300

    Arquivos aula

---
+++ pastinha/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Menu from './componentes/menu/Menu'
+import Conteudo from './componentes/conteudo/Conteudo'
+
+function App() {
+  return (
+    <div>
+      <Menu/>
+      <Conteudo/>
+    </div>
+  );
+}
+
+export default App;
